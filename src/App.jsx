02becbdf8c88b7ba44480fde2c6ba4140fbbf13a
Trang Dang <trangttdang01@gmail.com>
2024-03-11 13:47:27 -0400

    add readme

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ const App = () => {
   return (
     <div className="App">
       <div className="header">
-        <h1>Guess K-pop Groups</h1>
-        <h1>✨✨✨✨✨✨✨</h1>
-        <FlashCardContainer flashcards={cards}/>
+        <h1 style={{ color: "white" }}>Who is it?</h1>
+        <h2 style={{ color: "white" }}>Given the song names, try to guess the group name!</h2>
+        <h2 style={{ color: "white" }}>Total cards display: 10</h2>
+        <h2>✨✨✨✨✨✨✨</h2>
+        <FlashCardContainer flashcards={cards} />
       </div>
     </div>
   )
